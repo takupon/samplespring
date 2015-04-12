5a8184449f7c4f178ec6e443962278a69b29515b
仁井田 拓也 <takuya2230@gmail.com>
2015-04-12 09:55:26 +0900

    コミット

---
+++ src/main/java/samplespring/MeInfoService.java
@@ -0,0 +1,17 @@
+package samplespring;
+
+import org.springframework.dao.support.DaoSupport;
+
+public class MeInfoService extends DaoSupport {
+	
+	public void searchMe(){
+		
+	}
+
+	@Override
+	protected void checkDaoConfig() throws IllegalArgumentException {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+}
